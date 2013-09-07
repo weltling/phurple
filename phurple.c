930383cbfae836a01885b0d5875bc44ea5f9d59e
Anatol Belski <ab@php.net>
2013-09-07 20:17:42 +0200

    fixed uninitialized memory access

--- phurple.c
+++ phurple.c
@@ -736,6 +736,8 @@ call_custom_method(zval **object_pp, zend_class_entry *obj_ce,
 		}
 		fcic.calling_scope = obj_ce;
 		fcic.object_ptr = *object_pp;
+		/* XXX should probably pass per additional param */
+		fcic.called_scope = NULL;
 
 		result = zend_call_function(&fci, &fcic TSRMLS_CC);
 	}
